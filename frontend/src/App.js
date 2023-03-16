d20215f586edf75b13f42db2f6b700c975074f3e
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2023-03-16 11:40:03 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Form from "./components/Form";
+import UserList from "./components/UserList";
+
+function App() {
+  return (
+    <div>
+      <Form />
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
