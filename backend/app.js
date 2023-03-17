d3c98f8909b28962d21c446fbd716e4aee46ab72
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2023-03-17 17:26:53 +0530

    cors origin link fix

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000/", "https://full-stack-crud.vercel.app/"],
+    origin: ["http://localhost:3000", "https://full-stack-crud.vercel.app"],
   })
 );
 
