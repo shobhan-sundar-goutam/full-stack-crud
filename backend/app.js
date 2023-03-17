53227047ffb459560a912fbd17ebcadbf6d21d60
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2023-03-17 17:21:13 +0530

    cors test 2

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000/",
+  })
+);
 
 connect();
 
